81adb4ac19dc37033d05bfad3fa61d0a5d3b58ad
maurogdodata23 <maurogdatastudy@gmail.com>
2025-12-09 21:21:07 -0500

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,13 @@
+print("Hola Mundo")
+print("Esta es una prueba de un archivo Python.")
+
+caballo = 10
+Liebre = 5
+
+if caballo > Liebre:
+    print("El caballo es más rápido que la liebre.")
+
+    castor = 8
+    if castor < caballo:
+        print("El castor es más lento que el caballo.") 
+
---
+++ Trabajo_final/Index.py
@@ -0,0 +1,4 @@
+#import pandas as pd
+#datos = 'https://github.com/Twoeme/Prueba2/raw/refs/heads/main/Protein_Supply_Quantity_Data.csv'
+#df = pd.read_csv(datos)
+#df.info()
